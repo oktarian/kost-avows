a0e4fd874aa4640181aface6e01ef79cc882e28a
oktarian <rian15194@gmail.com>
2025-11-23 22:43:41 +0700

    fix: add missing imports

--- transaction-service/src/main/java/com/kostavows/transaction/config/SecurityConfig.java
+++ transaction-service/src/main/java/com/kostavows/transaction/config/SecurityConfig.java
@@ -8,6 +8,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -43,5 +47,9 @@ public class SecurityConfig {
             .formLogin(form -> form.disable());
 
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
